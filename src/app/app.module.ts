9ef114fe4f0d3c00c1848580166a1741504c8dec
EM <EM@WIN10AEM>
2023-03-17 16:15:30 -0400

    #7 Mettre en place la zone Prénom du formulaire Déclarer un problème

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ProblemeComponent } from './probleme/probleme.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProblemeComponent } from './probleme/probleme.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
@@ -8,6 +9,7 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
@@ -17,7 +19,42 @@ describe('ProblemeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('#1 | Zone PRÉNOM invalide avec 2 caractères',()=>{
+    let prenom = component.problemeForm.controls['prenom'];
+    prenom.setValue('a'.repeat(2))
+    expect(prenom.valid).toBeFalsy();
   });
+
+  it('#2 | Zone PRÉNOM valide avec 3 caractères',() =>{
+    let prenom = component.problemeForm.controls['prenom'];
+    prenom.setValue('a'.repeat(3));
+    expect(prenom.valid).toBeTruthy();
+  });
+
+  it('#3 | Zone PRÉNOM valide avec 200 caractères',() =>{
+    let prenom = component.problemeForm.controls['prenom'];
+    prenom.setValue('a'.repeat(200));
+    expect(prenom.valid).toBeTruthy();
+  });
+
+  it('#4 | Zone PRÉNOM invalide avec aucune valeur',() =>{
+    let prenom = component.problemeForm.controls['prenom'];
+  
+    expect(prenom.valid).toBeFalsy();
+  });
+
+  it('#5 | Zone PRÉNOM valide avec 10 espaces',() =>{
+    let prenom = component.problemeForm.controls['prenom'];
+    prenom.setValue(' '.repeat(10));
+    expect(prenom.valid).toBeTruthy();
+  });
+
+  it('#6 | Zone PRÉNOM valide avec 2 espaces et 1 caractère',() =>{
+    let prenom = component.problemeForm.controls['prenom'];
+    prenom.setValue(' '.repeat(2) + 'a'.repeat(1));
+    expect(prenom.valid).toBeTruthy();
+  });
+
+
+
 });
