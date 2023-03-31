2ff4c7dbb0af8a3e3d804a16d5ec7ca8e1248ebd
EM <EM@WIN10AEM>
2023-03-31 15:00:11 -0400

    Travail #9 | Faire évoluer la vue avec le nouveau validateur

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -21,7 +21,8 @@ export class ProblemeComponent implements OnInit {
   ngOnInit() {
     this.problemeForm = this.fb.group({
      //prenom: ['',[Validators.minLength(3),Validators.required]]
-     prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
+     prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]]
+     
     });
   }
 
