512caf266378a201a280998ee5e6391e453eb5b2
EM <EM@WIN10AEM>
2023-04-12 14:39:16 -0400

    Travail #11 | Contrôler les validations selon la notification demandée – Partie #1

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,9 +1,8 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { ProblemeComponent } from './probleme.component';
-
+import { ProblemeService } from './probleme.service';
 
 describe('ProblemeComponent', () => {
   let component: ProblemeComponent;
@@ -12,49 +11,80 @@ describe('ProblemeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, HttpClientModule],
-      declarations: [ ProblemeComponent ]
-    })
-    .compileComponents();
+      declarations: [ProblemeComponent],
+      providers: [ProblemeService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProblemeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('#1 | Zone PRÉNOM invalide avec 2 caractères',()=>{
+  it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
     let prenom = component.problemeForm.controls['prenom'];
-    prenom.setValue('a'.repeat(2))
+    prenom.setValue('a'.repeat(2));
     expect(prenom.valid).toBeFalsy();
   });
 
-  it('#2 | Zone PRÉNOM valide avec 3 caractères',() =>{
+  it('#2 | Zone PRÉNOM valide avec 3 caractères', () => {
     let prenom = component.problemeForm.controls['prenom'];
     prenom.setValue('a'.repeat(3));
     expect(prenom.valid).toBeTruthy();
   });
 
-  it('#3 | Zone PRÉNOM valide avec 200 caractères',() =>{
+  it('#3 | Zone PRÉNOM valide avec 200 caractères', () => {
     let prenom = component.problemeForm.controls['prenom'];
     prenom.setValue('a'.repeat(200));
     expect(prenom.valid).toBeTruthy();
   });
 
-  it('#4 | Zone PRÉNOM invalide avec aucune valeur',() =>{
+  it('#4 | Zone PRÉNOM invalide avec aucune valeur', () => {
     let prenom = component.problemeForm.controls['prenom'];
-  
+
     expect(prenom.valid).toBeFalsy();
   });
 
-  it('#5 | Zone PRÉNOM invalide avec 10 espaces',() =>{
+  it('#5 | Zone PRÉNOM invalide avec 10 espaces', () => {
     let prenom = component.problemeForm.controls['prenom'];
     prenom.setValue(' '.repeat(10));
     expect(prenom.valid).toBeFalsy();
   });
 
-  it('#6 | Zone PRÉNOM invalide avec 2 espaces et 1 caractère',() =>{
+  it('#6 | Zone PRÉNOM invalide avec 2 espaces et 1 caractère', () => {
     let prenom = component.problemeForm.controls['prenom'];
     prenom.setValue(' '.repeat(2) + 'a'.repeat(1));
     expect(prenom.valid).toBeFalsy();
   });
 
+  it('#15 | Zone TELEPHONE est désactivée quand ne pas me notifier', () => {
+    component.gestionNotifications('');
+
+    let zone = component.problemeForm.get('telephone');
+
+    expect(zone.status).toBeTruthy();
+  });
+
+  it('#16 | Zone TELEPHONE est vide quand ne pas me notifier', () => {
+    component.gestionNotifications('');
+
+    let zone = component.problemeForm.get('telephone');
+
+    expect(zone.value).toBeNull();
+  });
+
+  it('#17 | Zone ADRESSE COURRIEL est désactivée quand ne pas me notifier', () => {
+    component.gestionNotifications('');
+
+    let zone = component.problemeForm.get('courrielGroup.courriel');
+
+    expect(zone.disabled).toBeTruthy();
+  });
+
+  it('#18 | Zone CONFIRMER COURRIEL est désactivée quand ne pas me notifier', () => {
+    component.gestionNotifications('');
+
+    let zone = component.problemeForm.get('courrielGroup.courrielConfirmation');
+
+    expect(zone.disabled).toBeTruthy();
+  });
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VerifierCaracteresValidator } from '../shared/longueur-minimum/longueur-minimum.component';
 import { ITypeProbleme } from './probleme';
 import { ProblemeService } from './probleme.service';
@@ -7,35 +7,60 @@ import { ProblemeService } from './probleme.service';
 @Component({
   selector: 'Inter-probleme',
   templateUrl: './probleme.component.html',
-  styleUrls: ['./probleme.component.css']
+  styleUrls: ['./probleme.component.css'],
 })
 export class ProblemeComponent implements OnInit {
   problemeForm: FormGroup;
   typesProbleme: ITypeProbleme[];
   errorMessage: string;
 
+  save(): void {}
 
-  save(): void {
-  }
-
-  constructor(private fb: FormBuilder, private problemes: ProblemeService){
-  
-  
-
-  }
+  constructor(private fb: FormBuilder, private problemes: ProblemeService) {}
   ngOnInit() {
     this.problemeForm = this.fb.group({
-     //prenom: ['',[Validators.minLength(3),Validators.required]]
-    prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
-    nom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
-    noProbleme: ['', Validators.required]
-
+      //prenom: ['',[Validators.minLength(3),Validators.required]]
+      prenom: [
+        '',
+        [VerifierCaracteresValidator.longueurMinimum(3), Validators.required],
+      ],
+      nom: [
+        '',
+        [VerifierCaracteresValidator.longueurMinimum(3), Validators.required],
+      ],
+      noProbleme: ['', Validators.required],
+      noTypeProbleme: ['', Validators.required],
+      courrielGroup: this.fb.group({
+        courriel: [{ value: '', disabled: true }],
+        courrielConfirmation: [{ value: '', disabled: true }],
+      }),
+      telephone: [{ value: '', disabled: true }],
     });
 
-    this.problemes.obtenirTypesProbleme()
-        .subscribe(typesProbleme => this.typesProbleme = typesProbleme,
-                   error => this.errorMessage = <any>error);
+    this.problemes.obtenirTypesProbleme().subscribe(
+      (typesProbleme) => (this.typesProbleme = typesProbleme),
+      (error) => (this.errorMessage = <any>error)
+    );
+  }
+  gestionNotifications(typesProbleme: string): void {
+    const telephone = this.problemeForm.get('telephone');
+    const courriel = this.problemeForm.get('courrielGroup.courriel');
+    const courrielConfirmation = this.problemeForm.get(
+      'courrielGroup.courrielConfirmation'
+    );
+
+    // Tous remettre � z�ro
+    telephone.clearValidators();
+    telephone.reset(); // Pour enlever les messages d'erreur si le controle contenait des donn�es invaldides
+    telephone.disable();
+
+    courriel.clearValidators();
+    courriel.reset();
+    courriel.disable();
+
+    courrielConfirmation.clearValidators();
+    courrielConfirmation.reset();
+    courrielConfirmation.disable();
 
   }
-
 }
